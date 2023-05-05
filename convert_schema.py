39f49220d816ab8892cb8ff352fee05644964e99
lvferdi <tgdesrochers@gmail.com>
2023-05-05 09:58:47 -0400

    Update convert_schema.py

--- convert_schema.py
+++ convert_schema.py
@@ -1,28 +1,30 @@
-osquery_schema = json.load(open('osquery.schema'))
+import json 
 
-es_schema = {
-'mappings': {}
-}
+osquery_schema = json.load(open('osquery.schema'))  
 
-for table_name, columns in osquery_schema.items():
-if isinstance(columns, list):
-es_schema['mappings'][table_name] = {
-'properties': {}
-}
-for col in columns:
-column_name, column = list(col.items())[0]
-data_type = column_type_mapping[column['type']]
-es_schema['mappings'][table_name]['properties'][column_name] = {
-'type': data_type
-}
-else:
-es_schema['mappings'][table_name] = {
-'properties': {}
-}
-for column_name, column in columns.items():
-data_type = column_type_mapping[column['type']]
-es_schema['mappings'][table_name]['properties'][column_name] = {
-'type': data_type
-}
+es_schema = {  
+    'mappings': {}  
+}  
 
-json.dump(es_schema, open('es_schema.json','w'))
+for table_name, columns in osquery_schema.items():  
+    if isinstance(columns, list): 
+        es_schema['mappings'][table_name] = {  
+            'properties': {}  
+        }  
+        for col in columns: 
+            column_name, column = list(col.items())[0] 
+            data_type = column_type_mapping[column['type']] 
+            es_schema['mappings'][table_name]['properties'][column_name] = {  
+                'type': data_type  
+            }  
+    else: 
+        es_schema['mappings'][table_name] = {  
+            'properties': {}  
+        }  
+        for column_name, column in columns.items(): 
+            data_type = column_type_mapping[column['type']] 
+            es_schema['mappings'][table_name]['properties'][column_name] = {  
+                'type': data_type  
+            }  
+
+json.dump(es_schema, open('es_schema.json','w')) 
